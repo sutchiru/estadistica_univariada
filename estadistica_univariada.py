21603a7e0002441b3e4bc226eeb9a6efe078188c
Javiera Molina <javiera.molina.e@usach.cl>
2024-10-29 14:45:23 -0300

    Update estadistica_univariada.py

--- estadistica_univariada.py
+++ estadistica_univariada.py
@@ -12,12 +12,30 @@ def promedio(lista):
     promedio : float
     """
     
-    vals= []
+    vals_in= []
     for v in lista:
         if math.isfinite(v):
-            vals.append(v)
+            vals_in.append(v)
         
-    promedio=sum(vals)/len(vals)
+    promedio=sum(vals_in)/len(vals_in)
     return promedio
+    
+def mediana(vals_in):
+    #se eliminan valores que sean NaNs
+    vals=[]
+    for v in vals_in:
+        if math.isfinite(v):
+            vals.append(v)
+#ordenar la lista
+vals.sort()
+
+if len(vals)% !=0:
+    k= len(vals)//2
+    mediana= vals[k]            
+else: 
+    k= len(vals)//2 
+    mediana= (vals[k-1]+ vals[k])
+return mediana
+
   
 
